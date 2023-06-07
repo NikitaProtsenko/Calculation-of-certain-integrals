15be4510dd306d1466b6481779fe3d300ac490f6
Fivochnik <131523362+Fivochnik@users.noreply.github.com>
2023-06-07 11:39:16 +0700

    Update 9 Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,6 @@
-from importlib.machinery import all_suffixes
-from turtle import width, window_width
 from ForGame import *
 from math import sin, cos, tan, asin, acos, atan, sinh, cosh, log, pi, e
-
 from pyautogui import prompt
-
- 
- 
 
 #Missing math functions:
 def cotan(x):
@@ -95,6 +89,7 @@ zoom_koef - speed of zoom."""
         """None."""
         return
 
+#Camera of screen:
 MainCamera = camera(size = (WIDTH / 100, HEIGHT / 100),
                     speed = 0.05,
                     zoom_koef = 1.125)
@@ -208,6 +203,8 @@ cuts - splitting of visible segment of function."""
             y = func(x)
         except:
             y = None
+        if not type(y) in [int, float]:
+            y = None
         ps += [(x, y)]
     for i in range(1, len(ps)):
         if ps[i - 1][1] is None or ps[i][1] is None:
@@ -240,6 +237,8 @@ sample - x point of segment [0; 1] of calculating."""
             y = func(x + dx * sample)
         except:
             y = None
+        if not type(y) in [int, float]:
+            y = None
         ps += [(x, y)]
     for i in range(1, len(ps)):
         x1, y1 = ps[i]
@@ -255,16 +254,15 @@ sample - x point of segment [0; 1] of calculating."""
                 size = (x2 - x1, y2 - y1),
                 color = cl)
 
-#Globals for onStart function:
-f_str = None
-f = None
-c = None
-x1 = None
-x2 = None
-samp = None
-
 def integral_calculate_rect(func: function, start: float = 0, end: float = 10, cuts: int = 100, sample: float = 0):
+    """Return value of integral of function and False if function exactly has a singularity else True.
+func - function of one var x,
+start - bottom limit of integral,
+end - top limit of integral,
+cuts - splitting of segment,
+sample - x point of segment [0; 1] of calculating."""
     S = 0
+    prev_y = None
     flag = True
     if start > end:
         value,flag = integral_calculate_rect(func,end,start,cuts,sample)
@@ -276,33 +274,63 @@ def integral_calculate_rect(func: function, start: float = 0, end: float = 10, c
             continue
         try:
             y = func(x + dx * sample)
-            S = S + dx*y;    
+            if not type(y) in [int, float]:
+                y = None
+                flag = False
+            else:
+                if not prev_y is None and abs(y - prev_y) > maxdy * dx:
+                    flag = False
+                S = S + dx*y
         except:
             # In case of uncertainty, we output False.  
             # This means that the integral can be calculated incorrectly.
             flag = False
-    return (S,flag)
-def onStart():
-    global f,f_str, c, x1, x2, samp
-    f_str = prompt(text='Enter the function:', title='Function' , default='sin(x)')
-    f = new_function(f_str)
-    c = 100
-    x1 = float(prompt(text = 'Enter x1', title = 'x1', default = '0'))
-    x2 = float(prompt(text = 'Enter x2', title = 'x2', default = '10'))
-    samp = 0.5
+    return S, flag
 
-def draw_text(surf, text, size, x, y, color):
+def draw_text(surf: game.Surface, text: str, size: int, x: int, y: int, color: (int, int, int)):
+    """Draw text on surface surf.
+surf - surface for drawning,
+text - text for drawning,
+size - font size,
+x, y - position of drawning,
+color - RGB-color."""
     font = game.font.Font(None, size)
     text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
+    text_rect.topleft = (x, y)
     surf.blit(text_surface, text_rect)
+
+#Globals for onStart function:
+f_str = None
+f = None
+c = None
+x1 = None
+x2 = None
+samp = None
+maxdy = None
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+RED = (255,0,0)
+
+def onStart():
+    """Start function."""
+    global f,f_str, c, x1, x2, samp
+    f_str = prompt(text = 'Enter the function:',
+                   title = 'Function',
+                   default='sin(x)')
+    f = new_function(f_str)
+    c = 100
+    x1 = float(prompt(text = 'Enter x1',
+                      title = 'x1',
+                      default = '0'))
+    x2 = float(prompt(text = 'Enter x2',
+                      title = 'x2',
+                      default = '10'))
+    maxdy = 1000
+    samp = 0.5
 
 if __name__ == '__main__':
     onStart()
-    WHITE = (255,255,255)
-    GREEN = (0,255,0)
-    RED = (255,0,0)
     result = None
     while running:
         clock.tick(FPS)
@@ -336,6 +364,29 @@ if __name__ == '__main__':
                 
                 if event.key == game.K_RIGHT or event.key == game.K_d:
                     actions['right'] = False
+
+                if event.key == game.K_f:
+                    f_str = prompt(text = 'Enter the function:',
+                                   title = 'Function',
+                                   default = f_str)
+                    f = new_function(f_str)
+                    result = None
+
+                if event.key == game.K_r:
+                    x1 = float(prompt(text = 'Enter start of integral',
+                                      title = 'Region',
+                                      default = str(x1)))
+                    x2 = float(prompt(text = 'Enter end of integral',
+                                      title = 'Region',
+                                      default = str(x2)))
+                    result = None
+
+                if event.key == game.K_m:
+                    c = int(prompt(text = 'Enter the spliting:',
+                                   title = 'Spliting',
+                                   default = str(c)))
+                    result = None
+                    
             #Mouse wheel:
             if event.type == game.MOUSEWHEEL:
                 if event.y == 1:
@@ -347,7 +398,12 @@ if __name__ == '__main__':
                     MainCamera.speed *= MainCamera.zoom_koef
             #Mouse button down:
             if event.type == game.MOUSEBUTTONDOWN:
-                if event.button in [1, 2, 3]:
+                if event.button == 1:
+                    #x1 = ((event.pos[0] * 2 / WIDTH - 1) * MainCamera.size[0] + MainCamera.pos[0]) / 2
+                    x1 = MainCamera.x + (event.pos[0] - WIDTH / 2) * MainCamera.size[0] / WIDTH
+                    mouse_but[1] = True
+                    result = None
+                if event.button in [2, 3]:
                     mouse_but[event.button] = True
             #Mouse button up:
             if event.type == game.MOUSEBUTTONUP:
@@ -375,10 +431,19 @@ if __name__ == '__main__':
         if actions['right']:
             MainCamera.x += MainCamera.speed
 
+        #Select integral region:
+        if mouse_but[1]:
+            #x2 = ((getMousePos()[0] * 2 / WIDTH - 1) * MainCamera.size[0] + MainCamera.pos[0]) / 2
+            x2 = MainCamera.x + (getMousePos()[0] - WIDTH / 2) * MainCamera.size[0] / WIDTH
+            result = None
+
+        #For redraw:
         for i in all_lines.sprites():
             i.delete()
         for i in all_rects.sprites():
             i.delete()
+
+        #Redraw objects:
         draw_function(func = f,
                       cuts = c)
         draw_integral_rects(func = f,
@@ -386,17 +451,30 @@ if __name__ == '__main__':
                             end = x2,
                             cuts = c,
                             sample = samp)
+
+        #Integral value:
         if result is None :
             result = integral_calculate_rect(func = f,
-                            start = x1,
-                            end = x2,
-                            cuts = c,
-                            sample = samp) 
+                                             start = x1,
+                                             end = x2,
+                                             cuts = c,
+                                             sample = samp)
 
-        
-
-        AllUpdate(screen, all_sprites, (0, 0, 0))
-        draw_text(screen, "F(x) = "+ f_str ,20,100,20,WHITE)
-        draw_text(screen,"RESULT : "+str(result[0]),20,100,60,GREEN if result[1] else RED)
+        #Draw all:
+        AllUpdate(screen, all_sprites, (0, 0, 0), flip = False)
+        t = 'F(x) = ' + f_str
+        draw_text(surf = screen,
+                  text = t,
+                  size = 20,
+                  x = 10,
+                  y = 20,
+                  color = WHITE)
+        t = 'RESULT : ' + str(result[0])
+        draw_text(surf = screen,
+                  text = t,
+                  size = 20,
+                  x = 10,
+                  y = 40,
+                  color = GREEN if result[1] else RED)
         game.display.flip()
     game.quit()
